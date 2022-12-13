4f017e29964e3b035add235d5438f217af8b60af
Markoz Pena <markdrew53@gmail.com>
2022-12-12 19:03:55 -0600

    Create reset SVG icon

---
+++ components/icons/ResetIcon.tsx
@@ -0,0 +1,20 @@
+const ResetIcon = () => (
+  <svg
+    data-testid="geist-icon"
+    fill="none"
+    height="24"
+    shape-rendering="geometricPrecision"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="1.5"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M23 4v6h-6" />
+    <path d="M1 20v-6h6" />
+    <path d="M3.51 9a9 9 0 0114.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0020.49 15" />
+  </svg>
+);
+
+export default ResetIcon;
