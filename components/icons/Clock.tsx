f1350336f3a0e5834254efebdab5b464457269b3
Markoz Pena <markdrew53@gmail.com>
2022-12-12 19:02:50 -0600

    Create clock SVG icon

---
+++ components/icons/Clock.tsx
@@ -0,0 +1,19 @@
+const ClockIcon = () => (
+  <svg
+    data-testid="geist-icon"
+    fill="none"
+    height="24"
+    shape-rendering="geometricPrecision"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="1.5"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <circle cx="12" cy="12" r="10" />
+    <path d="M12 6v6l4 2" />
+  </svg>
+);
+
+export default ClockIcon;
