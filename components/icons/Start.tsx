c208a4cd7d908024b07ac064329f82297252589b
Markoz Pena <markdrew53@gmail.com>
2022-12-12 19:03:21 -0600

    components/icons/Start.tsx

---
+++ components/icons/Start.tsx
@@ -0,0 +1,18 @@
+const StartIcon = () => (
+  <svg
+    data-testid="geist-icon"
+    fill="none"
+    height="24"
+    shape-rendering="geometricPrecision"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="1.5"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
+  </svg>
+);
+
+export default StartIcon;
