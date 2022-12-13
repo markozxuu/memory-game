44edf08f763ae2d0cc504833a214cc5744179bbc
Markoz Pena <markdrew53@gmail.com>
2022-12-12 18:50:31 -0600

    Create configuration for tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './ui/**/*.{ts,tsx}',
+    './content/**/*.{md,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
