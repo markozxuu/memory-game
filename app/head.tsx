e43dca796c62e975791113cf2ec7665186ac5e03
Markoz Pena <markdrew53@gmail.com>
2022-12-12 19:40:57 -0600

    Adjsuting the head

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,30 @@
 export default function Head() {
   return (
     <>
-      <title></title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <title>Memory Game</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width" />
+      <meta
+        name="description"
+        content="An open source application built using the new router, server components and everything new in Next.js 13."
+      />
+      <meta property="og:title" content="Memory game" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://challenge-justo.vercel.app" />
+      <meta
+        property="og:image"
+        content="https://challenge-justo.vercel.app/og.png"
+      />
+      <meta name="twitter:title" content="memorygame" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://challenge-justo.vercel.app"
+      />
+      <meta
+        name="twitter:image"
+        content="https://challenge-justo.vercel.app/og.png"
+      />
     </>
   );
 }
